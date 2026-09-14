60584c2588507727e2f36dd242ad34826f333f86
jaat-somi <jaatsomi@gmail.com>
2026-09-14 20:38:21 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,23 @@
+// sw.js - Service Worker for Platoon Digital Patrol
+const CACHE_NAME = 'Platoon-Patrol-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/logo.png'
+];
+
+// Install Event
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+// Fetch Event (Required for PWA Install Prompt)
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
+});
